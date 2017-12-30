aba1bbb3ec92bd9f10e4260aafc879dc4a0c4382
Iwona Kolanowska <33376863+hajczek@users.noreply.github.com>
2017-12-30 15:37:27 +0100

    New information about functionality.

--- script.js
+++ script.js
@@ -1,102 +1,98 @@
-// Functionalities, which we need for 'e-CookBooK'
+// Functionalities, which we need for 'e-Cook-BooK'
 
 
-// 1.0. Make a recipe
+// 1.0. Make a recipe.
 
 
-// 1.1. Display recipe title
+// 1.1. Display recipe title.
 
 
-// 1.2. Display category of recipe
+// 1.2. Display category of recipe.
 
 
-// 1.3. Display added ingredients with input elements for quantity and measure, and option 'delete'
+// 1.3. Display list of added ingredients with quantity and measure, and option 'delete'.
 
 
-// 1.4. Display added needed thing with option 'delete'
+// 1.4. Display list of added needed thing with option 'delete'.
 
 
-// 1.5. Display added image
+// 1.5. Display added image.
 
 
-// 1.6. Add description of recipe
+// 1.6. Add textarea form description of recipe.
 
 
-// 1.7. Choose and add image to recipe with option delete
+// 1.7. Add title to recipe.
 
 
-// 1.8. Add time needed to make recipe
 
+// 2.0.  Create categories of recipies.
 
-// 1.9. Add quantity of person for which is that recipe - this can be change by user and then the quantity of ingredients must be appropriately adapted
 
+// 2.1. Add category of recipe to list of categories of recipies.
 
-// 1.10. See recipe
 
+// 2.2. Add choosen category of recipies to recpie.
 
-// 1.11. Save recipe
 
+// 2.3. Display list of recipes from choosen category of recipies (in new box). Options: 1. Delete choosen recipe from list of recipies. 2. See choosen recipe.
 
-// 1.12. Edit recipe
 
+// 2.4. Delete category of recipies from list of categories of recipies.
 
-// 1.13. Change recipe
 
 
-// 1.14. Print to .pdf file
+// 3.0. Create categories of ingredients.
 
 
+// 3.1. Add category of ingredients to list of categories of ingredients.
 
-// 2.0.  Create categories of recipies
 
+// 3.2. Add ingredient to choosen category of ingredients.
 
-// 2.1. Add category of recipe to list of categories of recipies
 
+// 3.3. Display list of categories of ingredients with option 'See list'.
 
-// 2.2. Add category of recipies to recpie
 
+// 3.4. Display list of ingredients from choosen category (in new box) Options: 1. Add choosen ingredients to recipe. 2. Delete choosen ingredients from list of ingredients.
 
-// 2.3. Display list of recipes from choosen category of recipies
 
 
-// 2.4. Delete category of recipies from list of categories of recipies
+// 4.0. Create list of needed things.
 
 
+// 4.1. Add thing to list of needed things.
 
-// 3.0. Create categories of ingredients
 
+// 4.2. Display list of needed things (in new box). Options: 1. Add choosen thing to recipe. 2. Delete choosen things from list of needed things.
 
-// 3.1. Add category of ingredients to list of categories of ingredients
 
 
-// 3.2. Add ingredient to choosen category of ingredients
+// 5.0. Additional information:
 
 
-// 3.3. Display list of categories of ingredients
+// 5.1. Choose and add image to recipe with option delete.
 
 
-// 3.4. Display list of ingredients from choosen category
+// 5.2. Add time needed to make recipe.
 
 
-// 3.5. Add choosen ingredient to recipe
+// 5.3. Add quantity of persons for which is that recipe - this can be change by user and then the quantity of ingredients must be appropriately adapted in recipe.
 
 
-// 3.6. Delete choosen ingredient from list of ingredients
+// 6.0. Buttons:
 
 
+// 6.1. See recipe.
 
-// 4.0. Create list of needed things
 
+// 6.2. Save recipe.
 
-// 4.1. Add thing to list of needed things
 
+// 6.3. Edit recipe - on view 'Recipe'.
 
-// 4.2. Display list of needed things
 
+// 6.4. Change recipe - on view 'Recipe'.
 
-// 4.3. Add choosen thing from list of needed thing to recipe
 
-
-// 4.4. Delete choosen thing from list of needed thing
-
-
+// 6.5. Print to .pdf file - on view 'Recipe'.
