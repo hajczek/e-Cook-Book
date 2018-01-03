64cd4ff9099e4f810a97542198e299fb0e673f80
Iwona Kolanowska <33376863+hajczek@users.noreply.github.com>
2018-01-03 20:57:04 +0100

    Corrected files
    Unnecessary comments removed from the file.

--- script.js
+++ script.js
@@ -4,48 +4,39 @@
 // 1.0. Make a recipe.
 
 
-// 1.1. Add title to recipe.
+// 1.1. Display list of added ingredients with quantity and unit, and option 'delete'.
 
 
-// 1.2. Choose category of recipe.
+// 1.2. Display list of added needed things with option 'delete'.
 
-
-// 1.3. Display list of added ingredients with quantity and unit, and option 'delete'.
-
-
-// 1.4. Display list of added needed things with option 'delete'.
-
-
-// 1.5. Add textarea for description of recipe.
 
 
 
 // 2.0. Additional elements:
 
 
-// 2.1. Choose image to recipe with option delete.
+// 2.1. Display added image to recipe.
 
 
-// 2.2. Add time needed to make recipe.
-
-
-// 2.3. Add quantity of persons for which is that recipe - this can be change by user and then the quantity of ingredients must be appropriately adapted in recipe.
-
-// 2.4. Display added image.
+// 2.2. Quantity of persons for which is that recipe - this can be change by user and then the quantity of ingredients must be appropriately adapted to recipe.
 
 
 
 
-// 3.0.  Create categories of recipies.
+// 3.0.  Create categories of recipes.
 
 
-// 3.1. Add new category of recipe to list of categories of recipies.
+// 3.1. Add new category of recipe to list of categories of recipes.
 
 
-// 3.3. Display list of recipes from choosen category of recipies - button 'See recipies' (in popup). Options: 1. Delete choosen recipe from list of recipies. 2. See choosen recipe.
+// 3.2. Add choosen category of recipes to recipe.
 
 
-// 3.4. Delete category of recipies from list of categories of recipies.
+// 3.3. Display list of recipes from choosen category of recipes - button 'See recipes' (in popup). 
+
+
+// 3.4. Delete category of recipes from list of categories of recipes.
+
 
 
 
@@ -58,10 +49,8 @@
 // 4.2. Add new ingredient to choosen category of ingredients.
 
 
-// 4.3. Display list of categories of ingredients with option 'See list' and 'Delete'.
+// 4.3. Display list of ingredients (button 'See list') from choosen category (in popup).
 
-
-// 4.4. Display list of ingredients (button 'See list') from choosen category (in popup) Options: 1. Add choosen ingredients to recipe. 2. Delete choosen ingredients from list of ingredients.
 
 
 
@@ -71,7 +60,9 @@
 // 5.1. Add thing to list of needed things.
 
 
-// 5.2. Display list of needed things (button 'See list') (in popup). Options: 1. Add choosen thing to recipe. 2. Delete choosen things from list of needed things.
+// 5.2. Display list of needed things (button 'See list') (in popup). 
+
+
 
 
 // 6.0. Buttons:
@@ -84,6 +75,3 @@
 
 
 // 6.3. Edit recipe - on view 'Recipe'.
-
-
-// 6.4. Change recipe - on view 'Recipe'.
